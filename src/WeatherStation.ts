d5ce248c4e454aaec4241db1d3bb8f650a57c84d
mwolak07 <mwolak07@gmail.com>
2023-01-23 16:17:55 -0500

    updated weather station

--- src/WeatherStation.ts
+++ src/WeatherStation.ts
@@ -1,6 +1,15 @@
+import CurrentConditionsDisplay from './CurrentConditionsDisplay';
+import ForecastDisplay from './ForecastDisplay';
+import HeatIndexDisplay from './HeatIndexDisplay';
+import StatisticsDisplay from './StatisticsDisplay';
 import WeatherData from './WeatherData';
 
 const weatherData = new WeatherData();
+
+const currentConditionsObserver = new CurrentConditionsDisplay(weatherData);
+const forecastObserver = new ForecastDisplay(weatherData);
+const heatIndexObserver = new HeatIndexDisplay(weatherData);
+const statisticsObserver = new StatisticsDisplay(weatherData);
 
 weatherData.setMeasurements(80, 65, 30.4);
 
